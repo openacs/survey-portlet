b0b6f8fc106dc6330f6406a67b2c6373af68f604
Don Baccus <dhogaza@pacifier.com>
2006-08-08 21:27:15 +0000

    First shot at merging oacs-5-2 apps used in .LRN to HEAD.

---
+++ sql/postgresql/upgrade/upgrade-2.1.1-2.1.2d1.sql
@@ -0,0 +1,4 @@
+-- Internationalize Portlet Message Keys
+
+update portal_element_map set pretty_name ='#survey.lt_Survey_Administration#'  where pretty_name =  'Survey Administration';
+update portal_element_map set pretty_name ='#survey.Surveys#'  where pretty_name =  'Survey';
